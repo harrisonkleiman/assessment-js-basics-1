17c5322612a7024348ff839fc7032daf63e5cb33
Harrison Kleiman <harrikleiman@gmail.com>
2022-03-04 12:36:49 -0500

    "last commit"

--- gitInfo.js
+++ gitInfo.js
@@ -49,7 +49,8 @@ const gitCloneDefinition =
 */
 
 //CODE HERE
-
+gitStatusDefinition =
+  "displays the state of the working directory and the staging area. It lets you see which changes have been staged, which haven't, and which files aren't being tracked by Git."
 //////////////////PROBLEM 6////////////////////
 /*
     Create a variable called 'gitAddDefinition'.  
